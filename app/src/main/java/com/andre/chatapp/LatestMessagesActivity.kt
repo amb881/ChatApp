a332424275f469652df087b779d3b8fa1e868bd7
AndreBaptista <andre.m.b.1@hotmail.com>
2021-01-28 12:39:40 +0000

    Criação do menu (nav_menu) com opção para a nova atividade new messages e de deslogar da app Correção da action bar e nome da app

--- app/src/main/java/com/andre/chatapp/LatestMessagesActivity.kt
+++ app/src/main/java/com/andre/chatapp/LatestMessagesActivity.kt
@@ -1,11 +1,49 @@
 package com.andre.chatapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        setContentView(R.layout.activity_latest_messages)
+
+        verifyUserIsLoggedIn()
     }
+
+    //verificar se o utilizador está logado senão abrir RegisterActivity
+    private fun verifyUserIsLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if(uid == null){
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    //criação do menu da navigation bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return super.onCreateOptionsMenu( menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.menu_new_message -> {
+
+            }
+            R.id.menu_sign_out ->{
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
